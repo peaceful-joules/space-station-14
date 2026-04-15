55e7176d000ca78fdf1967ad295c5ce80a84155f
joules <pacifist.jules@gmail.com>
2026-04-15 23:32:53 +0300

    test

--- Content.Client/Shuttles/UI/ShuttleNavControl.xaml.cs
+++ Content.Client/Shuttles/UI/ShuttleNavControl.xaml.cs
@@ -220,11 +220,11 @@ public sealed partial class ShuttleNavControl : BaseShuttleControl
 
                 var gridCentre = Vector2.Transform(gridBody.LocalCenter, curGridToView);
 
-                var gridDistance = (gridBody.LocalCenter - xform.LocalPosition).Length();
+                var mapCoords = _transform.GetWorldPosition(gUid);
+                var gridDistance = Vector2.Distance(mapCoords + Vector2.Abs(new(gridBounds.Width, -gridBounds.Height)) / 2, xform.LocalPosition);
+
                 var labelText = Loc.GetString("shuttle-console-iff-label", ("name", labelName),
                     ("distance", $"{gridDistance:0.0}"));
-
-                var mapCoords = _transform.GetWorldPosition(gUid);
                 var coordsText = $"({mapCoords.X:0.0}, {mapCoords.Y:0.0})";
 
                 // yes 1.0 scale is intended here.
